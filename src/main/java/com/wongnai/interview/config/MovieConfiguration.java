e04caf4dfbb18608ad0b65b7ed20900777f6f927
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-04-12 11:14:41 +0700

    เสร็จข้อที่1

--- src/main/java/com/wongnai/interview/config/MovieConfiguration.java
+++ src/main/java/com/wongnai/interview/config/MovieConfiguration.java
@@ -2,12 +2,29 @@ package com.wongnai.interview.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class MovieConfiguration {
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
+
+		// reference : https://stackoverflow.com/a/44219832/10600042
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+		converter.setSupportedMediaTypes(Arrays.asList(new MediaType[]{MediaType.ALL}));
+		messageConverters.add(converter);
+		restTemplate.setMessageConverters(messageConverters);
+
+		return restTemplate;
 	}
 }
--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,10 @@
 package com.wongnai.interview.movie.external;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestOperations;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -12,7 +12,7 @@ public class MovieDataServiceImpl implements MovieDataService {
 			= "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
 
 	@Autowired
-	private RestOperations restTemplate;
+	private RestTemplate restTemplate;
 
 	@Autowired
 	private ObjectMapper objectMapper;
@@ -24,6 +24,7 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
 
-		return null;
+		ResponseEntity<MoviesResponse> response = restTemplate.getForEntity(MOVIE_DATA_URL, MoviesResponse.class);
+		return response.getBody();
 	}
 }
