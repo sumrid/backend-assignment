6eff8107339c5442e6620c8f4dd3aa5329112468
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-04-12 13:12:36 +0700

    เสร็จข้อที่2

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,13 +1,17 @@
 package com.wongnai.interview.movie.search;
 
-import java.util.List;
-
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.MovieSearchService;
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MovieDataService;
+import com.wongnai.interview.movie.external.MoviesResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.wongnai.interview.movie.Movie;
-import com.wongnai.interview.movie.MovieSearchService;
-import com.wongnai.interview.movie.external.MovieDataService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component("simpleMovieSearchService")
 public class SimpleMovieSearchService implements MovieSearchService {
@@ -19,6 +23,20 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+
+		String queryTextCap = StringUtils.capitalize(queryText);
+
+		MoviesResponse result = movieDataService.fetchAll();
+		List<Movie> movies = new ArrayList<>();
+
+		for(MovieData movieData : result){
+			List<String> name = Arrays.asList(movieData.getTitle().split(" "));
+			if(name.contains(queryTextCap)){
+				Movie movie = new Movie(movieData.getTitle(), movieData.getCast());
+				movies.add(movie);
+			}
+		}
+
+		return movies;
 	}
 }
